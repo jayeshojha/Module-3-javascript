846fc4f635a58d38e88af7437dde4bf86109a868
jayeshojha <jayeshojha2002@gmail.com>
2024-02-29 00:55:03 +0530

    css

---
+++ Task/simple calculator/p.js
@@ -0,0 +1,15 @@
+let a = 3
+let b = 4
+function addition(a,b){
+    console.log(a+b);
+}
+function multiple(a,b){
+    console.log(a*b);
+}
+function divide(a,b){
+    console.log(a/b);
+}
+function substract(a,b){
+    console.log(a-b);
+}
+substract(a,b)
